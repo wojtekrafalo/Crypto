874656d4cedf860eda67f697d74f9b7c1deb0c27
wojtekrafalo <wojtekrafalo3@gmail.com>
2020-03-22 19:39:04 +0100

    Added the solution of the first exercise.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            LCG generator = new LCG(1, 2, 10, 1);
+
+            String s = generator.generateString(10);
+            System.out.println(s);
+            System.out.println(generator.generateString(15));
+
+            if (LCG.isLCB("1_2_3_4_5_1_2_3_4_5_1_2_3_4_5", SolutionClass.Cycle))
+                System.out.println("Should have returned true.");
+
+            if (LCG.isLCB("1_2_4_8_16_32_0_0_0_0", SolutionClass.Cycle))
+                System.out.println("Should have returned true.");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
